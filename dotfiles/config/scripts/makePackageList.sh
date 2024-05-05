8375cb6e96032761f99e817743e42a8ac2bae54e
mdb-dev-io <gitlab@martindbarker.com>
2024-05-05 10:24:46 +0100

    hook rewrite

--- dotfiles/config/scripts/makePackageList.sh
+++ dotfiles/config/scripts/makePackageList.sh
@@ -8,7 +8,3 @@ HOSTNAME=$(hostname)
 
 # Run pacman command and save AUR packages to hostname-foreignpkglist.txt
 /usr/bin/pacman -Qqem > /home/martin/.config/packages/${HOSTNAME}-foreignpkglist.txt
-
-chmod 644 ${HOSTNAME}-foreignpkglist.txt
-
-chmod 644 ${HOSTNAME}-pkglist.txt
